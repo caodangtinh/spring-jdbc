78e0776da5b0cc41afdd2e667eec3fb9ad6dea6d
tinhcao <caodangtinh@gmail.com>
2017-11-13 00:28:34 +0700

    add flyway

---
+++ src/main/resources/db/migration/V2__inser_data.sql
@@ -0,0 +1,8 @@
+insert into test(id) values (1);
+insert into test(id) values (2);
+insert into test(id) values (3);
+insert into test(id) values (4);
+insert into test(id) values (5);
+insert into test(id) values (6);
+insert into test(id) values (7);
+insert into test(id) values (8);
